c2387fb42ded057de000fed90afcfe92b6acbaaf
Thomas Davie <tom@huntedcow.com>
2011-02-10 18:16:20 +0000

    Initial Commit

---
+++ CoreParseTests/CoreParseTests.h
@@ -0,0 +1,17 @@
+//
+//  CoreParseTests.h
+//  CoreParseTests
+//
+//  Created by Tom Davie on 10/02/2011.
+//  Copyright 2011 Hunted Cow Studios Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CoreParseTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ CoreParseTests/CoreParseTests.m
@@ -0,0 +1,33 @@
+//
+//  CoreParseTests.m
+//  CoreParseTests
+//
+//  Created by Tom Davie on 10/02/2011.
+//  Copyright 2011 Hunted Cow Studios Ltd. All rights reserved.
+//
+
+#import "CoreParseTests.h"
+
+
+@implementation CoreParseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CoreParseTests");
+}
+
+@end
