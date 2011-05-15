ac1a7dc14e1fad0c61e33b98b6cc7fe53eb49037
Tom Davie <tom.davie@gmail.com>
2011-05-15 16:24:45 +0100

    Added documentation for token recognisers.

--- CoreParse/Tokenisation/Token Recognisers/CPKeywordRecogniser.h
+++ CoreParse/Tokenisation/Token Recognisers/CPKeywordRecogniser.h
@@ -11,16 +11,68 @@
 #import "CPTokenRecogniser.h"
 #import "CPKeywordToken.h"
 
+/**
+ * The `CPKeywordRecogniser` class attempts to recognise a specific keyword in a token stream.
+ * 
+ * A keyword recogniser attempts to recognise a specific word or set of symbols.
+ * Keyword recognisers can also check that the keyword is not followed by specific characters in order to stop it recognising the beginnings of words.
+ */
 @interface CPKeywordRecogniser : NSObject <CPTokenRecogniser>
-{}
 
+///---------------------------------------------------------------------------------------
+/// @name Creating and Initialising a Keyword Recogniser
+///---------------------------------------------------------------------------------------
+
+/**
+ * Creates a Keyword Recogniser for a specific keyword.
+ * 
+ * @param keyword The keyword to recognise.
+ *
+ * @return Returns a keyword recogniser for the passed keyword.
+ */
 + (id)recogniserForKeyword:(NSString *)keyword;
+
+/**
+ * Creates a Keyword Recogniser for a specific keyword.
+ * 
+ * @param keyword The keyword to recognise.
+ * @param invalidFollowingCharacters A set of characters that may not follow the keyword in the string being tokenised.
+ *
+ * @return Returns a keyword recogniser for the passed keyword.
+ */
++ (id)recogniserForKeyword:(NSString *)keyword invalidFollowingCharacters:(NSCharacterSet *)invalidFollowingCharacters;
+
+/**
+ * Initialises a Keyword Recogniser to recognise a specific keyword.
+ * 
+ * @param keyword The keyword to recognise.
+ *
+ * @return Returns the keyword recogniser initialised to recognise the passed keyword.
+ */
 - (id)initWithKeyword:(NSString *)keyword;
 
-+ (id)recogniserForKeyword:(NSString *)keyword invalidFollowingCharacters:(NSCharacterSet *)invalidFollowingCharacters;
+/**
+ * Initialises a Keyword Recogniser to recognise a specific keyword.
+ * 
+ * @param keyword The keyword to recognise.
+ * @param invalidFollowingCharacters A set of characters that may not follow the keyword in the string being tokenised.
+ *
+ * @return Returns the keyword recogniser initialised to recognise the passed keyword.
+ */
 - (id)initWithKeyword:(NSString *)keyword invalidFollowingCharacters:(NSCharacterSet *)invalidFollowingCharacters;
 
+///---------------------------------------------------------------------------------------
+/// @name Configuring a Keyword Recogniser
+///---------------------------------------------------------------------------------------
+
+/**
+ * The keyword that the recogniser should attempt to recognise.
+ */
 @property (readwrite,retain) NSString *keyword;
+
+/**
+ * A set of characters that may not follow the keyword.
+ */
 @property (readwrite,retain) NSCharacterSet *invalidFollowingCharacters;
 
 @end
--- CoreParse/Tokenisation/Token Recognisers/CPTokenRecogniser.h
+++ CoreParse/Tokenisation/Token Recognisers/CPTokenRecogniser.h
@@ -10,8 +10,22 @@
 
 #import "CPToken.h"
 
+/**
+ * The `CPTokenRecogniser` protocol defines methods needed to recognise tokens in a string.
+ */
 @protocol CPTokenRecogniser <NSObject>
 
+@required
+/**
+ * Attempts to recognise a token at `tokenPosition` in `tokenString`.
+ * 
+ * If a token is successfully recognised, it should be returned, and `tokenPosition` advanced to after the consumed characters.
+ * If no valid token is found `nil` must be returned instead, and `tokenPosition` left unchanged.
+ * 
+ * @param tokenString The string in which to recognise tokens.
+ * @param tokenPosition The position at which to try to find the token.  On output, the position after the recognised token.
+ * @return Returns the token recognised.
+ */
 - (CPToken *)recogniseTokenInString:(NSString *)tokenString currentTokenPosition:(NSUInteger *)tokenPosition;
 
 @end
