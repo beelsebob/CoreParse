9fad102810229b4268675ab5888243c3b56d8af9
Stadelman, Stan <stan.stadelman@sap.com>
2016-08-30 22:01:09 -0700

    added iOS framework with shared scheme for Carthage, set no-arc flags

---
+++ CoreParseIOS/CoreParseIOS.h
@@ -0,0 +1,20 @@
+//
+//  CoreParseIOS.h
+//  CoreParseIOS
+//
+//  Created by Stadelman, Stan on 8/30/16.
+//  Copyright © 2016 In The Beginning... All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreParseIOS.
+FOUNDATION_EXPORT double CoreParseIOSVersionNumber;
+
+//! Project version string for CoreParseIOS.
+FOUNDATION_EXPORT const unsigned char CoreParseIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreParseIOS/PublicHeader.h>
+
+
+#import <CoreParseIOS/CoreParse.h>
